f2a0296b9c9c68a13456f86f4e0702085c26f45b
MOHAMED ASHFAQUE <super.ashfak@gmail.com>
2020-11-15 08:21:24 +0530

    First Commit

---
+++ Count_Word_Dict.py
@@ -0,0 +1,11 @@
+#Scratch
+counts = dict()
+print('Enter a line of text')
+line = input('')
+words = line.split()
+print('words :', words)
+print('counting...')
+for word in words:
+    counts[word] = counts.get(word, 0) + 1
+print('counts', counts)
+print(counts.keys())
